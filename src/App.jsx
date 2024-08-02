ae54f8b6ca7b79108a8a573392204dad4a1c3459
Ahmed Vally <ahmedvally711@gmail.com>
2024-08-02 13:49:57 +0200

    instlled all the depndencies
    edited the jsx files added async and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-function App() {
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
+export default function App() {
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
 }
-
-export default App
